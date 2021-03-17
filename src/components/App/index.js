e098210ee08c4b626f4ef5ac819366ea66b16ce9
NicolasVid <65497850+NicolasVid@users.noreply.github.com>
2021-03-17 08:34:43 +0100

    Redux

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import Counter from '../../containers/Counter';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <Counter />
+  </div>
+);
+
+// == Export
+export default App;
